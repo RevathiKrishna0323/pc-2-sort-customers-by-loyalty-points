3defac18c01b0558adff62839ab5b5ce3127b953
Revathi G <revathikrishna0323@gmail.com>
2022-09-11 20:20:22 +0530

    reformatted the code in CustomerService.java

--- src/main/java/com/jap/customers/CustomerService.java
+++ src/main/java/com/jap/customers/CustomerService.java
@@ -1,13 +1,32 @@
 package com.jap.customers;
 
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class CustomerService {
-    // This method returns the customer's loyalty points in sorted order
-public List<Integer> getListOfCustomersSortedByLoyaltyPoints(List<Customer> customerList, LoyaltyPointComparator loyaltyPointComparator){
+    public static void main(String[] args) {
+        CustomerService customerService = new CustomerService();
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(new Customer(10, "Abhishek", "Male", false, "Indore", 240));
+        customerList.add(new Customer(11, "Vaibhav", "Male", false, "Indore", 180));
+        customerList.add(new Customer(12, "Diwakar", "Male", false, "Indore", 200));
 
-    return null;
-}
+
+        System.out.println(customerService.getListOfCustomersSortedByLoyaltyPoints(customerList, new LoyaltyPointComparator()));
+    }
+
+    // This method returns the customer's loyalty points in sorted order
+    public List<Integer> getListOfCustomersSortedByLoyaltyPoints(List<Customer> customerList, LoyaltyPointComparator loyaltyPointComparator) {
+        List<Integer> integerList = new ArrayList<>();
+        Collections.sort(customerList, loyaltyPointComparator);
+        Iterator<Customer> iterator = customerList.iterator();
+        while (iterator.hasNext()) {
+            integerList.add(iterator.next().getLoyaltyPoints());
+        }
+        return integerList;
+    }
 
 }
